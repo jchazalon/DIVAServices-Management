dda387a2e4761e36c7a98a7b41b11d4cb4c1a140
Urs Zysset <UrsZysset@gmail.com>
2016-03-09 19:39:04 +0100

    Working on individual edit forms

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :algorithms do
     resources :algorithm_wizard, controller: 'algorithm_wizard'
+      resources :input_parameters
   end
 
 end
