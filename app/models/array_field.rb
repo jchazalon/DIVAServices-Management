c51ef1471c55bc7ad8f46bfe277d0c6116985368
Urs Zysset <UrsZysset@gmail.com>
2016-04-20 11:13:09 +0200

    Added remaining validations of input fields from DIVAService

--- app/models/array_field.rb
+++ app/models/array_field.rb
@@ -3,9 +3,24 @@ class ArrayField < Field
   content_attr :minItems, :integer
   content_attr :uniqueItems, :boolean
 
+  validate :minimal_items
+  validate :unique_items
+
+  def unique_items
+    if !self.value.empty? && self.minItems
+      errors.add(:value, "cannot contain a element twice!") if self.value.split(';').uniq.length != self.value.split(';').length
+    end
+  end
+
+  def minimal_items
+    if !self.value.empty? && self.minItems
+      errors.add(:value, "must contain at least #{self.minItems} element(s)!") if self.value.split(';').size < self.minItems
+    end
+  end
+
   def self.create_from_hash(k,v)
     params = super(k,v)
-    params['infoText'] += ' | Seperate the individual values with a ";"'
+    params['infoText'] += ' | Seperate the individual values with a ";" (No spaces between)'
     params['minItems'] = v['minItems']
     params['uniqueItems'] = v['uniqueItems']
     return params
