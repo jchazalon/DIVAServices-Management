3bc24cd449de23c750e37ab79e04a8dee800c46c
Urs Zysset <UrsZysset@gmail.com>
2016-05-09 18:23:18 +0200

    Added regex format support

--- app/models/string_field.rb
+++ app/models/string_field.rb
@@ -3,11 +3,19 @@ class StringField < Field
   content_attr :minLength, :integer
   content_attr :maxLength, :integer
   content_attr :format, :string
+  content_attr :regex, :string
 
   validate :minimal_length
   validate :maximal_length
+  validate :match_regex
   validates :value, email: { strict_mode: true, message: 'Invalid email address!' }, if: '!self.value.empty? && self.format && self.format.eql?("email")'
   validates :value, url: { allow_nil: true, allow_blank: true, no_local: true }, if: '!self.value.empty? && self.format && self.format.eql?("uri")'
+
+  def match_regex
+    if !self.value.empty? && self.format && self.format.eql?("regex") && self.regex
+      errors.add(:value, 'is a invalid value') unless self.value =~ Regexp.new(self.regex)
+    end
+  end
 
   def minimal_length
     if !self.value.empty? && self.minLength
@@ -26,6 +34,7 @@ class StringField < Field
     params['minLength'] = v['minLength']
     params['maxLength'] = v['maxLength']
     params['format'] = v['format']
+    params['regex'] = v['regex']
     return params
   end
 
