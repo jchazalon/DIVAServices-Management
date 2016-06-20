587d3fd948ce3ae5bce242e99698f364734e5744
Urs Zysset <UrsZysset@gmail.com>
2016-06-20 12:59:24 +0200

    Documented jobs

--- app/jobs/publish_algorithm_job.rb
+++ app/jobs/publish_algorithm_job.rb
@@ -1,8 +1,12 @@
+##
+# ActiveJob used to publicate/update _algorithms_ on the DIVAServices.
 class PublishAlgorithmJob < ActiveJob::Base
   queue_as :default
 
+  ##
+  # Main method that is launched with job instance. Published/Updates the given algorithm
   def perform(algorithm_id)
-    p 'Job2'
+    p 'Publication/Update Job started' if Rails.env.development?
     algorithm = Algorithm.find(algorithm_id)
     if algorithm
       begin
@@ -11,38 +15,44 @@ class PublishAlgorithmJob < ActiveJob::Base
         else
           publish(algorithm)
         end
-      rescue Errno::ECONNREFUSED => e
+      rescue Errno::ECONNREFUSED => error
         algorithm.set_status(:connection_error, 'Connection error, please try again.')
       end
-    else
-      algorithm.set_status(:error, 'Algorithm not found, does it still exist?')
     end
   end
 
+  #TODO fix duplicated code below
+
+  ##
+  # Publishes the given _algorithm_ to the DIVAServices
   def publish(algorithm)
     begin
       diva_algorithm = DivaServicesApi::Algorithm.publish(algorithm.to_schema)
-
       if diva_algorithm
         algorithm.update_attribute(:diva_id, diva_algorithm.id)
         algorithm.set_status(diva_algorithm.status_code, diva_algorithm.status_message)
       else
-        algorithm.set_status(:error, "Unknown error during publication, please try again.\n#{diva_algorithm}")
+        algorithm.set_status(:error, "Error during publication, please try again.\n#{diva_algorithm}")
       end
     rescue Exceptions::DivaServicesError => error
       algorithm.set_status(:error, error.message)
     end
-
   end
 
+  ##
+  # Updates the given _algorithm_ on the DIVAServices
   def update(algorithm)
-    old_diva_algorithm = DivaServicesApi::Algorithm.by_id(algorithm.diva_id)
-    diva_algorithm = old_diva_algorithm.update(algorithm.diva_id, algorithm.to_schema)
-    if diva_algorithm
-      algorithm.update_attribute(:diva_id, diva_algorithm.id)
-      algorithm.set_status(diva_algorithm.status_code, diva_algorithm.status_message)
-    else
-      algorithm.set_status(:error, "Unknown error during update, please try again.\n#{diva_algorithm}")
+    begin
+      old_diva_algorithm = DivaServicesApi::Algorithm.by_id(algorithm.diva_id)
+      diva_algorithm = old_diva_algorithm.update(algorithm.diva_id, algorithm.to_schema)
+      if diva_algorithm
+        algorithm.update_attribute(:diva_id, diva_algorithm.id)
+        algorithm.set_status(diva_algorithm.status_code, diva_algorithm.status_message)
+      else
+        algorithm.set_status(:error, "Error during update, please try again.\n#{diva_algorithm}")
+      end
+    rescue Exceptions::DivaServicesError => error
+      algorithm.set_status(:error, error.message)
     end
   end
 end
--- app/jobs/validate_algorithm_job.rb
+++ app/jobs/validate_algorithm_job.rb
@@ -1,8 +1,12 @@
+##
+# ActiveJob used to validate _algorithms_ on the DIVAServices.
 class ValidateAlgorithmJob < ActiveJob::Base
   queue_as :default
 
+  ##
+  # Main method that is launched with job instance. Validates the given algorithm.
   def perform(algorithm_id)
-    p 'Job1'
+    p 'Validation Job started' if Rails.env.development?
     algorithm = Algorithm.find(algorithm_id)
     if algorithm
       begin
@@ -12,11 +16,9 @@ class ValidateAlgorithmJob < ActiveJob::Base
         else
           algorithm.set_status(:validation_error, "Validation Error\nMessage: #{response['message']}")
         end
-        rescue Errno::ECONNREFUSED => e
-          algorithm.set_status(:connection_error, 'Connection error during publication, please try again.')
-        end
-      else
-        algorithm.set_status(:error, 'Unknown error during publication, please try again.')
+      rescue Errno::ECONNREFUSED => error
+        algorithm.set_status(:connection_error, 'Connection error during publication, please try again.')
+      end
     end
   end
 end
